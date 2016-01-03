83bb6d1d8310f2c6e705470c367bdfcfd39daf22
angelujan <pepe.lujan2@gmail.com>
2016-01-03 00:31:21 -0700

    Meta para responsividad y Focus de la forma de contacto.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Registralow | Registra tu marca en México a bajo costo por internet</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
